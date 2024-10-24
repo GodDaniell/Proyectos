f7efc26a3915bad757090a3e4e06e84fe661f77e
GodDaniell <danieleduardosm01@gmail.com>
2024-10-24 11:21:35 -0600

    Actualizando el proyecto de Lexico 1

---
+++ UNIDAD 1/Lexico_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lexico_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (Lexico l = new Lexico("prueba.cpp"))
+                {
+                    while (!l.finArchivo())
+                    {
+                        l.NextToken();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+    }
+}
+
+
