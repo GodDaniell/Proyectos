000b7b2447076e1f1cfecf285b5a424489a534d1
GodDaniell <danieleduardosm01@gmail.com>
2024-10-24 11:20:55 -0600

    Agregando calculo de columna y matriz TRAND

--- UNIDAD 3/Lexico3/Lexico.cs
+++ UNIDAD 3/Lexico3/Lexico.cs
@@ -15,7 +15,47 @@ namespace Lexico3
         const int F = -1;
         const int E = -2;
 
-
+        int[,] TRAND =
+        {
+            {  0,  1,  1, 33,  1, 12, 14,  8,  9, 10, 11, 23, 16, 16, 18, 20, 21, 26, 25, 27, 29, 32, 34,  0,  F, 33 },
+            {  F,  1,  1,  F,  1,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F },
+            {  F,  F,  2,  3,  5,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F },
+            {  E,  E,  4,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E },
+            {  F,  F,  4,  F,  5,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F },
+            {  E,  E,  7,  E,  E,  6,  6,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E },
+            {  E,  E,  7,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E },
+            {  F,  F,  7,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F },
+            {  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F },
+            {  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F },
+            {  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F },
+            {  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F },
+            {  F,  F,  F,  F,  F, 13,  F,  F,  F,  F,  F, 13,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F },
+            {  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F },
+            {  F,  F,  F,  F,  F,  F, 13,  F,  F,  F,  F, 13,  F,  F,  F,  F,  F,  F, 15,  F,  F,  F,  F,  F,  F,  F },
+            {  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F },
+            {  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F, 17,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F },
+            {  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F },
+            {  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F, 19,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F },
+            {  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F },
+            {  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F, 19,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F },
+            {  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F, 22,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F },
+            {  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F },
+            {  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F, 24,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F },
+            {  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F },
+            {  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F, 24,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F },
+            {  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F, 24,  F,  F,  F,  F,  F,  F, 24,  F,  F,  F,  F,  F,  F,  F },
+            { 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 28, 27, 27, 27, 27,  E, 27 },
+            {  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F },
+            { 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30 },
+            {  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E,  E, 31,  E,  E,  E,  E,  E },
+            {  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F },
+            {  F,  F, 32,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F },
+            {  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F },
+            {  F,  F,  F,  F,  F,  F,  F,  F,  F,  F,  F, 17, 36,  F,  F,  F,  F,  F,  F,  F,  F,  F, 35,  F,  F,  F },
+            { 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35,  0, 35, 35 },
+            { 36, 36, 36, 36, 36, 36, 36, 36, 36, 36, 36, 36, 37, 36, 36, 36, 36, 36, 36, 36, 36, 36, 36, 36, 36, 36 },
+            { 36, 36, 36, 36, 36, 36, 36, 36, 36, 36, 36, 36, 37, 36, 36, 36, 36, 36, 36, 36, 36, 36,  0, 36, 36, 36 },
+        };
         public Lexico()
         {
             linea = 0;
@@ -71,9 +111,56 @@ namespace Lexico3
             asm.Close();
         }
 
+        private int Columna(char c)
+        {
+            if (c == '\n')
+            {
+                return 23;
+            }
+            else if (finArchivo())
+            {
+                return 24;
+            }
+            else if (char.IsWhiteSpace(c))
+            {
+                return 0;
+            }
+            else if (char.ToLower(c) == 'e')
+            {
+                return 4;
+            }
+            else if (char.IsLetter(c))
+            {
+                return 1;
+            }
+            else if (char.IsDigit(c))
+            {
+                return 2;
+            }
+            return 25;
+        }
+
+        private void Clasificar(int estado)
+        {
+            switch (estado)
+            {
+                case 1:
+                    setClasificacion(Tipos.Identificador);
+                    break;
+
+                case 2:
+                    setClasificacion(Tipos.Numero);
+                    break;
+
+                case 3:
+                    setClasificacion(Tipos.Caracter);
+                    break;
+            }
+        }
+
         public void NextToken()
         {
-            char transicion;
+            char c;
             string buffer = "";
             int estado = 0;
 
@@ -83,22 +170,23 @@ namespace Lexico3
                 {
                     buffer = "";
                 }
-                transicion = (char)archivo.Peek();
-                // Console.Write(transicion);
-                estado = automata(transicion, estado);
+                c = (char)archivo.Peek();
+                // Console.Write(c);
+                estado = TRAND[estado, Columna(c)];
+                Clasificar(estado);
 
 
 
                 if (estado >= 0)
                 {
                     archivo.Read();
-                    if (transicion == '\n')
+                    if (c == '\n')
                     {
                         linea++;
                     }
                     if (estado > 0)
                     {
-                        buffer += transicion;
+                        buffer += c;
                     }
                 }
             }
@@ -210,5 +298,5 @@ Caracter ----------->
 
 Automata: Modelo matematico que representa una expresion regular a traves de un 
 GRAFO, para una maquina de estado finito que consiste en un conjunto de estados 
-bien definidos, un estado inicial, un alfabeto de entrada y una funcion de transicion 
+bien definidos, un estado inicial, un alfabeto de entrada y una funcion de c 
 */
--- UNIDAD 3/Lexico3/obj/Debug/net8.0/Lexico3.AssemblyInfo.cs
+++ UNIDAD 3/Lexico3/obj/Debug/net8.0/Lexico3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lexico3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51303ced4a42efc3fa1b5bdbeec42c1f4faed2e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2778b7985ceb93d6a40ea6c8af52e9a80f69144")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lexico3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lexico3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
